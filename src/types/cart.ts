b04ec02b1d50a6b8acb8838f377405cc39a8662d
Mikhail Istomin <redeemer@inbox.ru>
2019-07-17 22:53:58 +0300

    add cart types

---
+++ src/types/cart.ts
@@ -0,0 +1,12 @@
+import { Product } from './product';
+
+export interface CartItem {
+  product: Product,
+  amount: number;
+}
+
+export interface Cart {
+  items: CartItem[],
+  totalPrice: number,
+  totalAmount: number,
+}
