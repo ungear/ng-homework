8d4e3d30f1a656c1531613521706f2bff906b374
Mikhail Istomin <redeemer@inbox.ru>
2019-08-06 22:45:53 +0300

    show number of items in cart in header

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/shared/services/cart.service';
+import { Cart } from 'src/types/cart';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  itemsInCart = 0;
+  constructor(
+    private cartService: CartService
+  ) { }
 
   ngOnInit() {
+   this.cartService.cart$.subscribe(this.onCartUpdated.bind(this));
+  }
+
+  private onCartUpdated(c: Cart){
+    this.itemsInCart = c && c.items && c.items.length
+      ? c.items.length
+      : 0;
   }
 
 }
