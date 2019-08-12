d4fc47c1719e440df7123602a949caf98176b0fb
Mikhail Istomin <mikhail_istomin@epam.com>
2019-08-12 11:45:06 +0300

    add product page

--- src/app/products/components/product-details/product-details.component.ts
+++ src/app/products/components/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router"
+import { Product } from 'src/types/product';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-product-details',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.component.scss']
 })
 export class ProductDetailsComponent implements OnInit {
-
-  constructor() { }
+  product: Product;
+  constructor(
+    private route:ActivatedRoute,
+    private productService: ProductService
+  ) { }
 
   ngOnInit() {
+    let pId = parseInt(this.route.snapshot.paramMap.get('productId'));
+    this.product = this.productService.getProductById(pId)
   }
 
 }
--- src/app/products/services/product.service.ts
+++ src/app/products/services/product.service.ts
@@ -2,6 +2,33 @@ import { Injectable } from '@angular/core';
 
 import { Product, Category } from '../../../types/product';
 
+const PRODUCTS = [
+  {
+    id: 1,
+    name: 'SICP',
+    description: 'a programming book',
+    price: 10,
+    category: Category.Book,
+    isAvailable: true,
+  },
+  {
+    id: 2,
+    name: 'Cheese',
+    description: 'soft French cheese',
+    price: 1,
+    category: Category.Food,
+    isAvailable: true,
+  },
+  {
+    id: 3,
+    name: 'Apple',
+    description: 'a wormy apple',
+    price: 1,
+    category: Category.Food,
+    isAvailable: false,
+  }
+]
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,31 +37,10 @@ export class ProductService {
   constructor() { }
 
   getProducts(): Product[] {
-    return [
-      {
-        id: 1,
-        name: 'SICP',
-        description: 'a programming book',
-        price: 10,
-        category: Category.Book,
-        isAvailable: true,
-      },
-      {
-        id: 2,
-        name: 'Cheese',
-        description: 'soft French cheese',
-        price: 1,
-        category: Category.Food,
-        isAvailable: true,
-      },
-      {
-        id: 3,
-        name: 'Apple',
-        description: 'a wormy apple',
-        price: 1,
-        category: Category.Food,
-        isAvailable: false,
-      }
-    ];
+    return PRODUCTS;
+  }
+
+  getProductById(pId: number): Product{
+    return PRODUCTS.find(x => x.id === pId)
   }
 }
