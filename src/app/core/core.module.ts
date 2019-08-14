a9a86e1c6ffb93d811600ddaebde7928b70436b0
Mikhail Istomin <mikhail_istomin@epam.com>
2019-08-14 11:06:56 +0300

    show a link to the products list

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 
-import { CoreRoutingModule } from "./core-routing.module"
+import { CoreRoutingModule } from "./core-routing.module";
+import { NavigationComponent } from './components/navigation/navigation.component'
 
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, NavigationComponent],
   imports: [
     CommonModule,
     CoreRoutingModule,
   ],
-  exports: [HeaderComponent]
+  exports: [HeaderComponent, NavigationComponent]
 })
 export class CoreModule { }
