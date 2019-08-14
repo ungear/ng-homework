8b9545d177ce682e5be8d308995dbaf02abf753a
Mikhail Istomin <mikhail_istomin@epam.com>
2019-08-14 11:02:59 +0300

    update cart data when amount changes

--- src/app/cart/components/cart-list/cart-list.component.ts
+++ src/app/cart/components/cart-list/cart-list.component.ts
@@ -1,19 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CartService } from 'src/app/shared/services/cart.service';
-import { CartItem } from 'src/types/cart';
+import { CartItem, Cart } from 'src/types/cart';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cart-list',
   templateUrl: './cart-list.component.html',
   styleUrls: ['./cart-list.component.scss']
 })
-export class CartListComponent implements OnInit{
+export class CartListComponent implements OnInit, OnDestroy{
   cartItems: CartItem[] = [];
   totalPrice: number;
   totalAmount: number;
+  destroy$ = new Subject<Boolean>();
   constructor(
     private cartService: CartService,
-  ) {}
+  ) {
+    this.cartService.cart$
+      .pipe(
+        takeUntil(this.destroy$)
+      )
+      .subscribe(this.onCartUpdated.bind(this))
+  }
 
   ngOnInit(){
     this.cartItems = this.cartService.currentCart.items;
@@ -27,6 +36,16 @@ export class CartListComponent implements OnInit{
 
   onCartItemDecrementAmount(ci: CartItem) {
     this.cartService.removeProductFromCart(ci.product);
+  }
+
+  ngOnDestroy(){
+    this.destroy$.next(true);
+    this.destroy$.complete();
+  }
+  private onCartUpdated(c: Cart){
+    this.cartItems = this.cartService.currentCart.items;
+    this.totalPrice = this.cartService.currentCart.totalPrice;
+    this.totalAmount = this.cartService.currentCart.totalAmount;
   }
 
 }
